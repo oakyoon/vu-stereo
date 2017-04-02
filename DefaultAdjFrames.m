5990edcc4e93cdfb79850e4d02516defa11e8a4f
Oakyoon Cha <oakyoon@gmail.com>
2017-04-02 20:11:00 +0900

    changed default viewing distance

--- DefaultAdjFrames.m
+++ DefaultAdjFrames.m
@@ -1,7 +1,7 @@
 function adj_frames = DefaultAdjFrames(pxpd)
-	% default pxpd: 400¡¿300 mm (1280¡¿960 px) screen at 600 mm distance 
+	% default pxpd: 400¡¿300 mm (1280¡¿960 px) screen at 650 mm distance 
 	if ~exist('pxpd', 'var') || isempty(pxpd)
-		pxpd = (600 * tand(.5) * 2) * (1280 / 400);
+		pxpd = (650 * tand(.5) * 2) * (1280 / 400);
 	end
 	adj_frames = DotFrames_Grid(pxpd);
 end
